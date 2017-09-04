d5ac322eddd898cf1b74b7357f85ffbe8977f02a
Alec Ortega <alecortega@live.com>
2017-09-04 18:46:12 -0400

    Fix fetch if exists in url already

--- client/src/redux/sagas/fetchPaletteSaga.js
+++ client/src/redux/sagas/fetchPaletteSaga.js
@@ -9,33 +9,31 @@ import url from 'utils/url';
 const isInitialCallSelector = state => likedColorsSelector(state).length === 0;
 
 export function* fetchPaletteGenerator() {
+  yield put(setIsFetching(true));
+
   const isInitialCall = yield select(isInitialCallSelector);
   const paletteFromUrl = url.getColors().reverse();
 
-  if (isInitialCall && !!paletteFromUrl.length) {
-    yield put(receivePalette(paletteFromUrl));
+  try {
+    const palette = yield call(PaletteAPI.getRandom);
 
-    for (let i = paletteFromUrl.length; i >= 1; i--) {
-      yield put(addLikedColor(paletteFromUrl[i - 1]));
-    }
-  } else {
-    try {
-      yield put(setIsFetching(true));
+    yield put(receivePalette(palette));
+    yield put(setIsFetching(false));
+    yield put(setIsStale(false));
 
-      const palette = yield call(PaletteAPI.getRandom);
-
-      yield put(receivePalette(palette));
-      yield put(setIsFetching(false));
-      yield put(setIsStale(false));
-
-      if (isInitialCall) {
+    if (isInitialCall) {
+      if (paletteFromUrl.length) {
+        for (let i = paletteFromUrl.length; i >= 1; i--) {
+          yield put(addLikedColor(paletteFromUrl[i - 1]));
+        }
+      } else {
         yield put(addLikedColor(palette[0]));
         const newPalette = yield select(likedColorsSelector);
         yield url.setColors(newPalette);
       }
-    } catch (e) {
-      yield put({ type: 'USER_FETCH_FAILED', message: e.message });
     }
+  } catch (e) {
+    yield put({ type: 'USER_FETCH_FAILED', message: e.message });
   }
 }
 
