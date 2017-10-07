2da062ebe355517ca2c515d06527d8a83689ebb0
Alec Ortega <aortega@hubspot.com>
2017-10-07 15:53:02 -0400

    Update dislike color epic

--- client/src/redux/epics/dislikeColor.js
+++ client/src/redux/epics/dislikeColor.js
@@ -3,22 +3,31 @@ import { Observable } from 'rxjs/Observable';
 import { DISLIKE_COLOR } from '../actions/ActionTypes';
 import likedColorsSelector from '../selectors/likedColorsSelector';
 import dislikedColorsSelector from '../selectors/dislikedColorsSelector';
+import suggestedColorSelector from '../selectors/suggestedColorSelector';
 import PaletteAPI from '../../api/PaletteAPI';
 import type { ReduxStore } from '../reducers/rootReducer';
 import { receivePalette } from '../actions/suggestedColors';
+import { changeLikedColor } from '../actions/likedColors';
 import Raven from 'raven-js';
 
 const dislikeColor = (action$, store) => {
   return action$.ofType(DISLIKE_COLOR)
-    .switchMap(() => {
+    .switchMap(({ payload }) => {
       const state: ReduxStore = store.getState();
 
-      Observable.fromPromise(PaletteAPI.getWithColors(
+      return Observable.fromPromise(PaletteAPI.getWithColors(
         likedColorsSelector(state),
         dislikedColorsSelector(state)
       ))
       .catch(err => Observable.of(Raven.captureException(err)))
-      .map((response) => receivePalette(response))
+      .flatMap((response) => {
+        const { hexCode } = suggestedColorSelector(state);
+
+        return [
+          receivePalette(response),
+          changeLikedColor({ color: payload, newHexCode: hexCode })
+        ];
+      })
     });
 };
 
--- client/src/redux/epics/rootEpic.js
+++ client/src/redux/epics/rootEpic.js
@@ -2,10 +2,12 @@
 import { combineEpics } from 'redux-observable';
 import fetchInitialPalette from './fetchInitialPalette';
 import setPaletteUrl from './setPaletteUrl';
+import dislikeColor from './dislikeColor';
 import likeColor from './likeColor';
 
 export const rootEpic = combineEpics(
   fetchInitialPalette,
   likeColor,
-  setPaletteUrl
+  setPaletteUrl,
+  dislikeColor
 );
