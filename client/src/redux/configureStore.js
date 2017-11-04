8ceb5d510ca66fb754ff172ea4bc6f1c458688d2
Alec Ortega <aortega@hubspot.com>
2017-11-04 14:16:40 -0400

    Remove sagas

--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -1,13 +1,10 @@
 // @flow
 import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware from 'redux-saga';
 import { createEpicMiddleware } from 'redux-observable';
 import { rootReducer } from './reducers/rootReducer';
 import type { ReduxStore } from '../constants/FlowTypes';
 import { rootEpic } from './epics/rootEpic';
-import rootSaga from './sagas/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
 const epicMiddleware = createEpicMiddleware(rootEpic);
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
