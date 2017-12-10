040f6e2bde70360db67d7ffec9d44a3cd2df4dec
Alec Ortega <aortega@hubspot.com>
2017-12-10 18:41:16 -0500

    make copy more intuitive

--- client/src/UILibrary/input/UITextCopyInput.js
+++ client/src/UILibrary/input/UITextCopyInput.js
@@ -1,22 +1,49 @@
 // @flow
 import React from 'react';
+import keyMirror from 'keymirror';
 import CSSModules from 'react-css-modules';
 import styles from './UITextCopyInput.css';
-import UITextInput from './UITextInput';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+const DISPLAY_STATES = keyMirror({
+  INACTIVE: null,
+  ACTIVE: null,
+});
+
+const ACTIVE_STATE_DURATION = 1250;
 
 type Props = {
   +value: mixed,
   +onChange: () => {},
 };
 
-class UITextCopyInput extends React.Component<Props> {
+type State = {
+  displayState: 'INACTIVE' | 'ACTIVE',
+};
+
+class UITextCopyInput extends React.Component<Props, State> {
+  state = {
+    displayState: DISPLAY_STATES.INACTIVE,
+  };
+
   static defaultProps = {
     onChange: () => {},
-  }
+  };
+
+  handleClick = () => {
+    this.setState({ displayState: DISPLAY_STATES.ACTIVE });
+
+    setTimeout(() => {
+      this.setState({ displayState: DISPLAY_STATES.INACTIVE });
+    }, ACTIVE_STATE_DURATION);
+  };
 
   render() {
+    const { displayState } = this.state;
     const { value, onChange } = this.props;
+    const buttonCopy =
+      displayState === DISPLAY_STATES.INACTIVE ? 'Copy' : 'Copied';
+
     return (
       <div styleName="text-copy-input-container">
         <input
@@ -26,7 +53,9 @@ class UITextCopyInput extends React.Component<Props> {
           styleName="text-copy-input"
         />
         <CopyToClipboard text={value}>
-          <button styleName="copy-button">Copy</button>
+          <button onClick={this.handleClick} styleName="copy-button">
+            {buttonCopy}
+          </button>
         </CopyToClipboard>
       </div>
     );
