0ea57d73e9187b387e1c92accd86c9dcfba860aa
Alec Ortega <aortega@hubspot.com>
2017-12-10 18:26:16 -0500

    Update url when mistype

--- client/src/redux/epics/setPaletteUrl.js
+++ client/src/redux/epics/setPaletteUrl.js
@@ -4,12 +4,22 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/ignoreElements';
 import 'rxjs/add/observable/empty';
 import { Observable } from 'rxjs/Observable';
-import { CHANGE_LIKED_COLOR, ADD_LIKED_COLOR, REMOVE_LIKED_COLOR } from '../actions/ActionTypes';
+import {
+  CHANGE_LIKED_COLOR,
+  ADD_LIKED_COLOR,
+  ADD_LIKED_COLORS,
+  REMOVE_LIKED_COLOR,
+} from '../actions/ActionTypes';
 import { browserHistory } from 'react-router';
 import url from '../../utils/url';
 import likedColorsSelector from '../selectors/likedColorsSelector';
 
-const updatableActions = [CHANGE_LIKED_COLOR, ADD_LIKED_COLOR, REMOVE_LIKED_COLOR];
+const updatableActions = [
+  CHANGE_LIKED_COLOR,
+  ADD_LIKED_COLOR,
+  ADD_LIKED_COLORS,
+  REMOVE_LIKED_COLOR,
+];
 
 const setPaletteUrl = (action$, store) => {
   return action$
@@ -18,7 +28,7 @@ const setPaletteUrl = (action$, store) => {
       const newPalette = likedColorsSelector(store.getState());
       browserHistory.push(url.stringifyColors(newPalette));
 
-      return Observable.empty()
+      return Observable.empty();
     })
     .ignoreElements();
 };
