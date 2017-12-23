bd7b65775eedc924baa48e9d28906b4bee32bc3c
Alec Ortega <aortega@hubspot.com>
2017-12-23 09:51:21 -0500

    Add error handling to random api client

--- client/src/redux/observables/fetchRandomPalette.js
+++ client/src/redux/observables/fetchRandomPalette.js
@@ -2,11 +2,12 @@
 import { Observable } from 'rxjs/Observable';
 import PaletteAPI from '../../api/PaletteAPI';
 import type { ReduxStore } from '../../constants/FlowTypes';
+import Raven from 'raven-js';
 
-const fetchRandomPalette = (
-  state: ReduxStore,
-) => {
-  return Observable.fromPromise(PaletteAPI.getRandom())
+const fetchRandomPalette = (state: ReduxStore) => {
+  return Observable.fromPromise(PaletteAPI.getRandom()).catch(err => {
+    return Observable.of(Raven.captureException(err.data.error));
+  });
 };
 
 export default fetchRandomPalette;
